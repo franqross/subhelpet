fd7bbb5a1c2ed0e1f978d32c68a7c9c90671317a
francojof <francojof2@gmail.com>
2022-06-16 00:54:06 -0400

    asdsda

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -103,7 +103,7 @@ export const captureOrder =async (req,res) =>{
         },
     });
     if (response.data.status=='COMPLETED'){
-        console.log(response.data.purchase_units);
+        console.log(response.data.purchase_units[0]);
         console.log(token);
         var todayDate = new Date();
         const toDate = new Date();
