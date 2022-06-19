c504eeba0eae552952135b322357ec22ac63c357
francojof <francojof2@gmail.com>
2022-06-18 21:54:32 -0400

    f hasta sub

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -89,11 +89,11 @@ const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders`,order,{
     db.query(`SELECT f_hasta FROM subscripcion WHERE id_subscripcion ='${id_sub}'`, function (err, result, fields){
         if (err) throw err;
         else{
-               /*  console.log(typeof result[0].f_hasta); */
-                let f_hastaUsuario = result[0].f_hasta;
-                let f_actual = new Date();
+                console.log(typeof result[0].f_hasta );
+                /* let f_hastaUsuario = result[0].f_hasta;
+                let f_actual = new Date(); */
                 
-                console.log("FECHA HASTA DE SUSCRIPCION USUARIO QUE PAGA"); 
+             /*    console.log("FECHA HASTA DE SUSCRIPCION USUARIO QUE PAGA"); 
                 console.log(typeof f_hastaUsuario); 
                 console.log(typeof f_actual); 
                
@@ -102,7 +102,7 @@ const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders`,order,{
              } else {
                 console.log('suscripcion activa, retornar error.');
                 return res.console.log('redireccionar aca');
-             }
+             } */
         }
         
       });
