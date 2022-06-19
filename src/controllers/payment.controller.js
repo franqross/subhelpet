3e225ca81bddddaeca9537077cf30b3c229b33f8
francojof <francojof2@gmail.com>
2022-06-18 21:50:14 -0400

    f hasta sub

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -89,9 +89,10 @@ const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders`,order,{
     db.query(`SELECT f_hasta FROM subscripcion WHERE id_subscripcion ='${id_sub}'`, function (err, result, fields){
         if (err) throw err;
         else{
-                console.log(typeof result[0].f_hasta);
+               /*  console.log(typeof result[0].f_hasta); */
                 /* let f_hastaUsuario = result[0].f_hasta; */
                 let f_actual = new Date();
+                
                 console.log("FECHA HASTA DE SUSCRIPCION USUARIO QUE PAGA"); 
                 console.log(f_hastaUsuario); 
                 console.log(f_actual); 
